c6bfca8f502d5f859cf796bd5626196f50e95722
Syauqi <sauki084@gmail.com>
2026-07-20 15:07:47 +0700

    WPU webstore: Create Product Filament Resources

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductResource\Pages;
+use App\Models\Product;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+
+                Section::make()->schema([
+                    TextInput::make('name')->label('Product Name'),
+                    TextInput::make('sku')->label('SKU')->unique(),
+                    TextInput::make('slug')->label('Slug')->unique(),
+                    TextInput::make('stock')->label('Stock')->numeric()->default(0),
+                    TextInput::make('price')->label('Price')->numeric()->prefix('Rp'),
+                    TextInput::make('weight')->label('Weight')->numeric()->suffix('Gram'),
+                ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+}
