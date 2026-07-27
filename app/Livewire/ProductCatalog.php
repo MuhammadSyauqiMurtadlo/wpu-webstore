54d8fd45fefd6eb7d0c98a279096036525ddc98d
Syauqi <sauki084@gmail.com>
2026-07-27 23:14:42 +0700

    WPUstore : Passing Data To View (Basic)

--- app/Livewire/ProductCatalog.php
+++ app/Livewire/ProductCatalog.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class ProductCatalog extends Component
 {
     public function render()
     {
-        return view('livewire.product-catalog');
+        $products = Product::paginate(9);
+
+        // TODO make a DT0
+        return view('livewire.product-catalog', compact('products'));
     }
 }
