13ff8b843ea719d553cce03031c6ef8bd7307895
Syauqi <sauki084@gmail.com>
2026-07-30 14:43:32 +0700

    WPU webstore: Product Collection Data

--- app/Livewire/ProductCatalog.php
+++ app/Livewire/ProductCatalog.php
@@ -2,17 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Data\ProductCollectionData;
 use App\Data\ProductData;
 use App\Models\Product;
+use App\Models\Tag;
 use Livewire\Component;
 
 class ProductCatalog extends Component
 {
     public function render()
     {
+        $collection_result = Tag::query()->WithType('collection')->WithCount('products')->get();
         $result = Product::paginate(1); // ORM // Database Query
-        $products = ProductData::collect($result); // DTO (Data Transfer Object)
 
-        return view('livewire.product-catalog', compact('products'));
+        $products = ProductData::collect($result); // DTO (Data Transfer Object)
+        $collections = ProductCollectionData::collect($collection_result);
+
+        return view('livewire.product-catalog', compact('products', 'collections'));
     }
 }
