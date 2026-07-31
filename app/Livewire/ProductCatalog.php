11cc7f4648989d5fe7fd1695273b4a812e6dc1cb
Syauqi <sauki084@gmail.com>
2026-07-31 16:25:40 +0700

    WPU store: Product Catalog Search, Filter & Sorting

--- app/Livewire/ProductCatalog.php
+++ app/Livewire/ProductCatalog.php
@@ -7,15 +7,70 @@ use App\Data\ProductData;
 use App\Models\Product;
 use App\Models\Tag;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ProductCatalog extends Component
 {
+    use WithPagination;
+
+    public $queryString = [
+        'select_collections' => ['except' => []],
+        'sort_by' => ['except' => 'newest'],
+        'search' => ['except' => []],
+    ];
+
+    public array $select_collections = [];
+
+    public string $search = '';
+
+    public string $sort_by = 'newest'; // latest, price_asc, price_desc
+
+    public function applyFilters()
+    {
+        $this->resetPage();
+    }
+
+    public function resetFilters()
+    {
+        $this->select_collections = [];
+        $this->search = '';
+        $this->sort_by = 'newest';
+        $this->resetPage();
+    }
+
     public function render()
     {
         $collection_result = Tag::query()->WithType('collection')->WithCount('products')->get();
-        $result = Product::paginate(1); // ORM // Database Query
+        // $result = Product::paginate(1); // ORM // Database Query
 
-        $products = ProductData::collect($result); // DTO (Data Transfer Object)
+        $query = Product::query();
+        if ($this->search) {
+            $query->where('name', 'like', "%{$this->search}%");
+        }
+
+        if (! empty($this->select_collections)) {
+            $query->whereHas('tags', function ($q) {
+                $q->whereIn('id', $this->select_collections);
+            });
+        }
+
+        switch ($this->sort_by) {
+            case 'newest':
+                $query->latest();
+                break;
+            case 'price_asc':
+                $query->orderBy('price', 'asc');
+                break;
+            case 'price_desc':
+                $query->orderBy('price', 'desc');
+                break;
+            default:
+                $query->latest();
+        }
+
+        $products = ProductData::collect(
+            $query->paginate(9)
+        ); // DTO (Data Transfer Object)
         $collections = ProductCollectionData::collect($collection_result);
 
         return view('livewire.product-catalog', compact('products', 'collections'));
