bd21784e29f77566a3a9ea39c936aa2d987d8cbc
Syauqi <sauki084@gmail.com>
2026-08-08 22:09:51 +0700

    WPU store: Validation AddToCart

--- app/Livewire/AddToCart.php
+++ app/Livewire/AddToCart.php
@@ -28,10 +28,20 @@ class AddToCart extends Component
         $this->stock = $product->stock;
         $this->weight = $product->weight;
         $this->quantity = $cart->getItemBySku($product->sku)?->quantity ?? 1;
+
+        $this->validate();
+    }
+
+    protected function rules(): array
+    {
+        return [
+            'quantity' => ['required', 'integer', 'min:1', "max:{$this->stock}"],
+        ];
     }
 
     public function AddToCart(CartServiceInterface $cart)
     {
+        $this->validate();
         $cart->addOrUpdate(
             new CartItemData(
                 sku: $this->sku,
