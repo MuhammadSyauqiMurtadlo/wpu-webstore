71d14a57bbba08c5350848d42138a5aff7707cfb
Syauqi <sauki084@gmail.com>
2026-08-21 11:01:12 +0700

    WPU store: Search Query Service

--- app/Livewire/Checkout.php
+++ app/Livewire/Checkout.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Contract\CartServiceInterface;
 use App\Data\CartData;
 use App\Data\RegionData;
+use App\Service\RegionQueryService;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Number;
 use Livewire\Component;
@@ -72,42 +73,28 @@ class Checkout extends Component
         return $cart->all();
     }
 
-    public function getRegionsProperty(): DataCollection
+    public function getRegionsProperty(RegionQueryService $query_service): DataCollection
     {
-        $data = [
-            [
-                'code' => '001',
-                'province' => 'Jawa Barat',
-                'city' => 'Kota Bandung',
-                'district' => 'District',
-                'sub_district' => 'Sub District',
-                'postal_code' => '1234',
-            ],
-            [
-                'code' => '002',
-                'province' => 'Jawa Barat 1',
-                'city' => 'Kota Bandung 1',
-                'district' => 'District',
-                'sub_district' => 'Sub District',
-                'postal_code' => '12345',
-            ],
-        ];
-
         if (! data_get($this->region_selector, 'keyword')) {
             $data = [];
+
+            return new DataCollection(RegionData::class, $data);
         }
 
-        return new DataCollection(RegionData::class, $data);
+        return $query_service->searchRegionByName(
+            data_get($this->region_selector, 'keyword')
+        );
+
     }
 
-    public function getRegionProperty(): ?RegionData
+    public function getRegionProperty(RegionQueryService $query_service): ?RegionData
     {
         $region_selected = data_get($this->region_selector, 'region_selected');
         if (! $region_selected) {
             return null;
         }
 
-        return $this->regions->ToCollection()->first(fn (RegionData $region) => $region->code == $region_selected);
+        return $query_service->searchRegionByCode($region_selected);
     }
 
     public function updatedRegionSelectorRegionSelected($value)
